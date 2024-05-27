e8cf582498cb36f2e2b0109b8f1d10b97b979afe
SankalpKadam <sankalptp07@gmail.com>
2024-05-27 15:17:41 -0500

    create new navbar component

---
+++ src/components/UniversalComponents/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import Menu from './Menu/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
+
+  return (
+    <div className='h-full'>
+      <div className='text-darktext bg-darkaccentbg flex flex-row justify-between items-center p-3 lg:hidden'>
+                <div className='md:text-2xl'>Job Tracker</div>
+                <MenuIcon fontSize='large' onClick={()=>{
+                    menuOpen?setMenuOpen(false):setMenuOpen(true)
+                }}/>
+            </div>
+            <div className={`grow-0.3 lg:ml-3 rounded-md h-full lg:translate-y-0 transition ease-in-out delay-150 ${menuOpen?'translate-y-[-1%]':'xs:translate-y-[-120%] sm:translate-y-[-160%]'}`}>
+                <Menu />
+            </div>
+    </div>
+  )
+}
+
+export default Navbar
