77dd847533f37b672104af0a084be4fd1858b02e
SankalpKadam <sankalptp07@gmail.com>
2024-05-27 15:17:55 -0500

    adjusted menu and font size for responsiveness

--- src/components/UniversalComponents/Navbar/Menu/Menu.jsx
+++ src/components/UniversalComponents/Navbar/Menu/Menu.jsx
@@ -12,12 +12,12 @@ const Menu = () => {
                     <div className='lg:w-[90px] lg:h-[90px] md:w-[100px] md:h-[100px] w-[60px] h-[60px] rounded-full bg-darkcirclebg'>
                         <img src={require('../Menu/3d-illustration-person-with-sunglasses_23-2149436188.avif')} alt="" srcset="" className='rounded-full' />
                     </div>
-                    <div className='pl-4 my-auto text-3xl lg:text-sm sm:text-lg'>Hello, Sankalp</div>
+                    <div className='pl-4 my-auto text-3xl lg:text-base sm:text-lg xs:text-base md:text-2xl'>Hello, Sankalp</div>
                 </div>
             </div>
             <div className='grow-0.4 flex flex-col justify-center items-center border-b-2 border-darkcirclebg w-full h-[60%]'>
 
-                <Link to={"/"} className='text-darktext my-4 flex items-center'>
+                <Link to={"/"} className='text-darktext my-4 flex items-center md:text-2xl lg:text-base'>
                     {/* <div className='flex items-center'> */}
                     <div className='p-1 mr-2 bg-darkdashboardaccent rounded-md'>
                         <SpaceDashboardIcon fontSize='medium' />
@@ -25,22 +25,25 @@ const Menu = () => {
                     Dashboard
                     {/* </div> */}
                 </Link>
-                <Link to={"/"} className='text-darktext my-4 flex items-center'><div className='p-1 mr-2 bg-darkleetcodeaccentt rounded-md'>
-                    <DataObjectIcon fontSize='medium' />
-                </div>
+                <Link to={"/"} className='text-darktext my-4 flex items-center md:text-2xl lg:text-base'>
+                    <div className='p-1 mr-2 bg-darkleetcodeaccentt rounded-md'>
+                        <DataObjectIcon fontSize='medium' />
+                    </div>
                     LeetCoding</Link>
-                <Link to={"/"} className='text-darktext my-4 flex items-center'><div className='p-1 mr-2 bg-darkapplicationaccentt rounded-md'>
-                    <AppRegistrationIcon fontSize='medium' />
-                </div>
+                <Link to={"/"} className='text-darktext my-4 flex items-center md:text-2xl lg:text-base'>
+                    <div className='p-1 mr-2 bg-darkapplicationaccentt rounded-md'>
+                        <AppRegistrationIcon fontSize='medium' />
+                    </div>
                     Application</Link>
-                <Link to={"/"} className='text-darktext my-4 flex items-center'><div className='p-1 mr-2 bg-darknetworkingaccentt rounded-md'>
-                    <LinkedInIcon fontSize='medium' />
-                </div>
+                <Link to={"/"} className='text-darktext my-4 flex items-center md:text-2xl lg:text-base'>
+                    <div className='p-1 mr-2 bg-darknetworkingaccentt rounded-md'>
+                        <LinkedInIcon fontSize='medium' />
+                    </div>
                     Networking</Link>
                 {/* <Link to={"/"} className='text-darktext my-4'>Dashboard</Link> */}
             </div>
-            <div className='grow-0.3 h-[20%] justify-center items-center flex'>
-                <div className='rounded-md text-darktext p-3 border-2 border-darkcirclebg'>
+            <div className='grow-0.3 h-[20%] justify-center items-center flex sm:mt-6 mt-0'>
+                <div className='rounded-md text-darktext p-3 border-2 border-darkcirclebg md:text-2xl lg:text-base'>
                     Sign Out
                 </div>
             </div>
