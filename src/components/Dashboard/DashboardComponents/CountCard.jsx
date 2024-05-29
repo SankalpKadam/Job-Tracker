0fbd03d59c35bf44efcaf9fb1fc8254833857f80
SankalpKadam <sankalptp07@gmail.com>
2024-05-29 08:07:55 -0500

    added the button group

--- src/components/Dashboard/DashboardComponents/CountCard.jsx
+++ src/components/Dashboard/DashboardComponents/CountCard.jsx
@@ -4,18 +4,44 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const CountCard = ({title="Application",count="200"}) => {
   return (
-    <div className='bg-darkaccentbg w-[90%] p-4 h-[100%] rounded-2xl drop-shadow-md flex flex-col items-center justify-center'>
-      <div className='flex flex-col items-center justify-center w-full'>
-        <div className='text-darktext grow-[0.3]'>
+    <div className='bg-darkaccentbg 
+                    w-[90%] 
+                    p-4 
+                    h-[100%] 
+                    rounded-2xl 
+                    drop-shadow-md 
+                    flex 
+                    flex-col 
+                    items-center 
+                    justify-center'>
+      <div className='flex 
+                      flex-col 
+                      items-center 
+                      justify-center 
+                      w-full'>
+        <div className='text-darktext 
+                        grow-[0.3]'>
             {
                 title == "Application"?
                 <AppRegistrationOutlinedIcon fontSize='large' className='bg-darkapplicationaccent p-1 rounded-md'/>:<LinkedInIcon fontSize="large" className='bg-darkleetcodeaccent p-1 rounded-md'/>
             }
         </div>
-        <div className='text-darktext lg:text-xl border-b-2 border-darkcirclebg w-full flex items-center justify-center my-2 py-2 grow-[0.5]'>
+        <div className='text-darktext 
+                        lg:text-xl 
+                        border-b-2 
+                        border-darkcirclebg 
+                        w-full 
+                        flex 
+                        items-center 
+                        justify-center 
+                        my-2 
+                        py-2 
+                        grow-[0.5]'>
             {title}
         </div>
-        <div className='text-darktext lg:text-3xl grow-[0.2]'>
+        <div className='text-darktext 
+                        lg:text-3xl 
+                        grow-[0.2]'>
           200
         </div>
       </div>
--- src/components/Dashboard/DashboardPage.jsx
+++ src/components/Dashboard/DashboardPage.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Navbar from '../UniversalComponents/Navbar/Navbar'
 import CountCard from './DashboardComponents/CountCard'
 const DashboardPage = () => {
+
+    //Manipulating button background and color
+    const [active, setActive] = useState('graph');
+
+
+
     return (
         <div className='dashboardpage 
                         bg-darkbg 
@@ -31,6 +37,43 @@ const DashboardPage = () => {
                                 lg:text-3xl
                                 font-semibold'>
                     Job Search Overview
+                </div>
+                <div className='w-full'>
+
+                    <div className='flex
+                                flex-row
+                                text-darktext
+                                bg-darkaccentbg
+                                w-[200px]
+                                my-4
+                                rounded-3xl
+                                justify-start
+                                items-center
+                                '>
+                        <div className={`border-r-2 
+                                        border-darkcirclebg 
+                                        transition
+                                        delay-150
+                                        rounded-3xl
+                                        p-2 
+                                        w-[100px] 
+                                        text-center 
+                                        ${active == "flow" ? "" : "bg-darktext text-darkbg "}`}
+                            onClick={() => setActive('graph')}
+                        >
+                            Graph
+                        </div>
+                        <div className={`p-2 
+                                        w-[100px] 
+                                        text-center
+                                        transition
+                                        delay-150
+                                        rounded-3xl
+                                        ${active == "flow" ? "bg-darktext text-darkbg" : ""}`}
+                            onClick={()=>setActive('flow')}>
+                            Flow
+                        </div>
+                    </div>
                 </div>
                 <div className='grow-[0.1] 
                                 flex
