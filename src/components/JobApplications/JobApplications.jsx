a2a4de1ae80bd5b8a93d6b28a6d4bc9cab3131a7
SankalpKadam <sankalptp07@gmail.com>
2024-05-29 17:08:06 -0500

    Create JobApplications.jsx

---
+++ src/components/JobApplications/JobApplications.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../UniversalComponents/Navbar/Navbar'
+const JobApplications = () => {
+  return (
+    <div className='bg-darkbg 
+                    lg:flex 
+                    lg:flex-row 
+                    flex 
+                    flex-col 
+                    h-[98.5%]'>
+            
+      
+    </div>
+  )
+}
+
+export default JobApplications
